7b1c554b4fbbc8f70c5615a4a154482bb5b4e0ac
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-04-18 23:01:58 +0000

    chore: continue development

--- packages/astro-integration-demo-components/astro.ts
+++ packages/astro-integration-demo-components/astro.ts
@@ -1,2 +1,4 @@
 export { default as Component } from './src/Component.astro';
 export { default as SnipcartSetup } from './src/SnipcartSetup.astro';
+export { default as SnipcartTotalPrice } from './src/SnipcartTotalPrice.astro';
+export { default as SnipcartItemsCount } from './src/SnipcartItemsCount.astro';
--- packages/astro-integration-demo-components/src/types.ts
+++ packages/astro-integration-demo-components/src/types.ts
@@ -2,3 +2,27 @@ export interface Foo {
 	readonly bar: string;
 	readonly qux: string;
 }
+
+export type SnipcartProductDimensions = {
+	readonly weightInGrams?: number;
+	readonly lengthInCentimeters?: number;
+	readonly heightInCentimeters?: number;
+	readonly widthInCentimeters?: number;
+};
+
+export type SnipcartProduct = {
+	readonly id: string;
+    readonly name: string;
+    readonly price: number;
+    readonly url?: string | URL;
+    readonly description?: string;
+    readonly image?: string | URL;
+    readonly categories?: readonly string[];
+    readonly metadata?: object;
+    readonly fileGuid?: string;
+    readonly quantity?: number;
+    readonly minimumQuanity?: number;
+    readonly maximumQuanity?: number;
+    readonly quantityStep?: number;
+    readonly dimensions?: SnipcartProductDimensions;
+};
